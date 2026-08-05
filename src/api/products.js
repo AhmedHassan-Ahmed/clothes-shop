2912deb438216adfbc6515128c235947b13a499b
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-08-05 16:45:37 +0300

    add-backend

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://crud-backend-api-flame.vercel.app/api",
 });
 
 export const getProductsApi = () => API.get("/products");
